2ca293f25d96215fd900199b424331f1ee239362
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-06-08 17:44:15 +0300

    commit tree 11b67c289bb0a23b962da2410ac0a0ab2f385e16

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+
+// A variable called multiply that has 62 as its result.
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+
+
+// for generating random integers greater than zero.
+const random = Math.floor(Math.random() * 10) + 1;
+
+
+
+//A variable that has remainder 4
+const num3 = 19;
+const num4 = 5;
+const mod = num3 % num4;
+
+
+
+// A variable that has 20 as maximum
+const max = Math.max(13, 12, 16, 18, 2, 4, 6, 20);
